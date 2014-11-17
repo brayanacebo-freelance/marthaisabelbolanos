393aa05ea0fc5e9bb7e57d8313374ef8194b9400
brayan acebo <brayanacebo@gmail.com>
2014-11-17 14:57:13 -0500

    merge

---
+++ system/cms/logs/log-2014-11-17.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-11-17 20:56:58 --> Severity: Warning  --> unlink(system/cms/cache/default/page_m/30b8d0a5a40b10c609e59aa77d5213e72ca608a4.cache): Permission denied /Applications/XAMPP/xamppfiles/htdocs/freelos/marthaisabelbolanos/system/codeigniter/helpers/file_helper.php 129
+ERROR - 2014-11-17 20:56:58 --> Severity: Warning  --> fopen(system/cms/cache/default/page_m/3d0f03d41abb2568755ed242724deda93c662da3.cache): failed to open stream: Permission denied /Applications/XAMPP/xamppfiles/htdocs/freelos/marthaisabelbolanos/system/cms/libraries/Pyrocache.php 399
+ERROR - 2014-11-17 20:56:58 --> Unable to write Cache file: system/cms/cache/default/page_m/3d0f03d41abb2568755ed242724deda93c662da3.cache
+ERROR - 2014-11-17 20:56:58 --> Severity: Warning  --> fopen(system/cms/cache/default/page_m/342ddf6b4185a9d011df0515492f5f03b0c2f833.cache): failed to open stream: Permission denied /Applications/XAMPP/xamppfiles/htdocs/freelos/marthaisabelbolanos/system/cms/libraries/Pyrocache.php 399
+ERROR - 2014-11-17 20:56:58 --> Unable to write Cache file: system/cms/cache/default/page_m/342ddf6b4185a9d011df0515492f5f03b0c2f833.cache
+ERROR - 2014-11-17 20:56:58 --> Severity: Warning  --> fopen(system/cms/cache/default/page_m/f0ca142041f6c8dfc1e95783ef567437bf08e337.cache): failed to open stream: Permission denied /Applications/XAMPP/xamppfiles/htdocs/freelos/marthaisabelbolanos/system/cms/libraries/Pyrocache.php 399
+ERROR - 2014-11-17 20:56:58 --> Unable to write Cache file: system/cms/cache/default/page_m/f0ca142041f6c8dfc1e95783ef567437bf08e337.cache
+ERROR - 2014-11-17 20:56:58 --> Page Missing: img/favicon.png
